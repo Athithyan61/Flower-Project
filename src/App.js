04168d18602fe73380071e73711e0c6b2febdaae
Shanmathi0605 <smily.shanvi6597@gmail.com>
2025-12-03 15:09:43 +0530

    nav

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
+import Contact from './Component/Contact';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Let's Start Our Project Now</h1>
-      <h2>All The Best Guys</h2>
-    </div>
+    <>
+     <Contact />
+    </>
   );
 }
 
---
+++ src/Component/Contact.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import styles from "../Styles/Contact.module.css";
+export default function Contact() {
+  return (
+    <>
+    <div className={styles.main}>
+        <h1>Contact Us</h1>
+    </div>
+    
+    
+    
+    </>
+  )
+}
